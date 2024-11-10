fe52cc0e891c2291eee5dae461c2c1e7dae4f13c
PaoloBaltazar <gpaolobaltazar@gmail.com>
2024-11-10 11:08:26 +0800

    update navbar font

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -27,19 +27,19 @@ const Nav = () => {
     <div className="lg:hidden block absolute top-16 w-full left-0 right-0 bg-slate-900 transition">
       <ul className="text-center text-xl p-20">
         <Link spy={true} smooth={true} to="Home">
-          <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Home</li>
+          <li className="font-primaryMedium my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Home</li>
         </Link>
         <Link spy={true} smooth={true} to="About">
-          <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">About</li>
+          <li className="font-primaryMedium my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">About</li>
         </Link>
         <Link spy={true} smooth={true} to="Skills">
-          <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Skills</li>
+          <li className="font-primaryMedium my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Skills</li>
         </Link>
         <Link spy={true} smooth={true} to="Projects">
-          <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Projects</li>
+          <li className="font-primaryMedium my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Projects</li>
         </Link>
         <Link spy={true} smooth={true} to="Contact">
-          <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Contact</li>
+          <li className="font-primaryMedium -4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Contact</li>
         </Link>
       </ul>
     </div>
@@ -49,26 +49,26 @@ const Nav = () => {
     <nav className={`fixed w-full z-50 transition-all duration-300 ${navBackground ? 'bg-slate-900' : 'bg-transparent'}`}>
       <div className="h-10vh flex justify-between text-white lg:py-5 px-20 py-4">
         <div className="flex items-center flex-1">
-          <span className="text-3xl font-bold">GPB</span>
+          <span className="font-primaryBold text-3xl font-bold">GPB</span>
         </div>
 
         <div className="lg:flex md:flex lg:flex-1 items-center justify-end font-normal hidden">
           <div className="flex-10">
             <ul className="flex gap-8 mr-16 text-[18px]">
               <Link spy={true} smooth={true} to="Home">
-                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Home</li>
+                <li className="font-primaryMedium hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Home</li>
               </Link>
               <Link spy={true} smooth={true} to="About">
-                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">About</li>
+                <li className="font-primaryMedium hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">About</li>
               </Link>
               <Link spy={true} smooth={true} to="Skills">
-                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Skills</li>
+                <li className="font-primaryMedium hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Skills</li>
               </Link>
               <Link spy={true} smooth={true} to="Projects">
-                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Projects</li>
+                <li className="font-primaryMedium hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Projects</li>
               </Link>
               <Link spy={true} smooth={true} to="Contact">
-                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Contact</li>
+                <li className="font-primaryMedium hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Contact</li>
               </Link>
             </ul>
           </div>
