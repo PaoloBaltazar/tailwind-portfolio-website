50532a91a352de37a0e0779addba9d29caec5dbc
PaoloBaltazar <gpaolobaltazar@gmail.com>
2024-11-11 21:53:02 +0800

    add background to home section

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,10 @@
 import profile from '../assets/profile.jpg';
 import { Spotlight } from "./ui/Spotlight";
+import { HoverBorderGradient } from "./ui/hover-border-gradient";
 
 const Home = () => {
   return (
-    <div className="h-lvh  rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+    <div className="h-lvh w-full dark:bg-black bg-white  dark:bg-grid-small-white/[0.2] bg-grid-small-black/[0.2] relative flex items-center justify-center">
       <Spotlight
         className="-top-40 left-0 md:left-50 md:-top-20"
         fill="white"
@@ -18,9 +19,16 @@ const Home = () => {
             Aspiring web developer passionate about creating dynamic and responsive websites. I specialize in HTML, CSS, JavaScript, and React, aiming to build efficient and user-friendly applications.
           </p>
           
-          <button className="px-4 py-2 bg-fuchsia-600 text-white rounded hover:bg-fuchsia-700">
-            Resume
-          </button>
+          <div>
+            <HoverBorderGradient
+              containerClassName="rounded-full"
+              as="button"
+              className="dark:bg-black bg-white text-black dark:text-white flex items-center space-x-2"
+            >
+              <AceternityLogo />
+              <span>Resume</span>
+            </HoverBorderGradient>
+          </div>
         </div>
 
         <div className='md:m-16 content-center'>
@@ -28,6 +36,27 @@ const Home = () => {
         </div>
       </section>
     </div>
+  );
+};
+
+const AceternityLogo = () => {
+  return (
+    <svg
+      width="66"
+      height="65"
+      viewBox="0 0 66 65"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-3 w-3 text-black dark:text-white"
+    >
+      <path
+        d="M8 8.05571C8 8.05571 54.9009 18.1782 57.8687 30.062C60.8365 41.9458 9.05432 57.4696 9.05432 57.4696"
+        stroke="currentColor"
+        strokeWidth="15"
+        strokeMiterlimit="3.86874"
+        strokeLinecap="round"
+      />
+    </svg>
   );
 };
 
