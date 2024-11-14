f0b63e3414512f7679fdea868e54e6ba0a8b0491
PaoloBaltazar <gpaolobaltazar@gmail.com>
2024-11-14 11:02:10 +0800

    add resume links

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -22,13 +22,18 @@ const Home = () => {
           <p className='font-primaryRegular mb-8 lg:text-lg'>
             Aspiring web developer passionate about creating dynamic and responsive websites. I specialize in HTML, CSS, JavaScript, and React, aiming to build efficient and user-friendly applications.
           </p>
+
+         
           
-          <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-            <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-              Download Resume
-            </span>
-          </button>
+          <a href="https://tinyurl.com/BaltazarGabrielPaolo-Resume" target="_blank" rel="noopener noreferrer">
+            <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+              <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+                Download Resume
+              </span>
+            </button>
+          </a>
+          
         </div>
 
         <div className='md:m-16 content-center'>
